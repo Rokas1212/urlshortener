873d751cb0a9852ca064ca23f3e018090c022be0
Rokas Čiuplinskas <rokas.roktantu@gmail.com>
2024-07-15 01:35:31 +0300

    add authentication

--- UrlShortener/Controllers/HomeController.cs
+++ UrlShortener/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using UrlShortener.Models;
 
 namespace UrlShortener.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,23 +1,32 @@
 using UrlShortener.Data;
 using Microsoft.EntityFrameworkCore;
 using UrlShortener.Services;
+using Microsoft.AspNetCore.Identity;
 
 
 namespace UrlShortener
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            builder.Services
+                .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
 
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+
+            builder.Services.AddRazorPages();
 
             // Register QrCodeService
             builder.Services.AddScoped<QrCodeService>();
@@ -39,6 +48,11 @@ namespace UrlShortener
 
             app.UseAuthorization();
 
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapRazorPages();
+            });
+
             // Default Route
             app.MapControllerRoute(
                 name: "default",
@@ -54,6 +68,61 @@ namespace UrlShortener
                 name: "redirect",
                 pattern: "/r/{key}",
                 defaults: new { controller = "URLShortener", action = "RedirectToOriginal" });
+
+
+
+            //Seeding roles
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                
+                var roles = new[] { "Admin", "User" };
+
+                foreach (var role in roles)
+                {
+
+                    if(!await roleManager.RoleExistsAsync(role))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+
+                }
+            }
+
+            //Seeding users
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                var admin = "admin@admin.com";
+                var user = "user@user.com";
+                var password = "Test123!";
+
+                //Seed admin account
+                if (await userManager.FindByEmailAsync(admin) == null)
+                {
+                    var newAdmin = new IdentityUser();
+                    newAdmin.UserName = admin;
+                    newAdmin.Email = admin;
+
+                    await userManager.CreateAsync(newAdmin, password);
+
+                    await userManager.AddToRoleAsync(newAdmin, "Admin");
+                }
+
+                //Seed user account
+                if (await userManager.FindByEmailAsync(user) == null)
+                {
+                    var newUser = new IdentityUser();
+                    newUser.UserName = user;
+                    newUser.Email = user;
+
+                    await userManager.CreateAsync(newUser, password);
+
+                    await userManager.AddToRoleAsync(newUser, "User");
+                }
+            }
 
             app.Run();
         }
